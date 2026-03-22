39168b0e1e5ece9ab1f4ae026ee01279fd4cd05a
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-03-22 08:21:02 +0000

    Letters except q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for letter in range(26):
-    if letter == 'e' or letter == 'q':
+    if chr(letter + 97) == 'e' or chr(letter +97) == 'q':
         letter += 1
     print("{}".format(chr(97 + letter)), end="")
