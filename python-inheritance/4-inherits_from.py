ea062222241735c4dd1768a38073ead85f53c2e2
ehmedlicelal <ehmedlicelal1@gmail.com>
2026-04-12 15:19:38 +0000

    Inheritance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -40,7 +40,7 @@ def inherits_from(obj, a_class):
     >>> inherits_from(obj, int)
     False
     """
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return False
     elif isinstance(obj, a_class):
         return True
